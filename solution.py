407b41e0106507e915c97fd9f4803960cc36b384
spremarartna <95597829+spremarartna@users.noreply.github.com>
2022-02-24 21:09:19 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,13 +5,12 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     endmsg = "\r\n.\r\n"
 
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-    mailserver = 'localhost'
-    port = 1025
+    mailserver = ('smtp.gmail.com',1025)
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
     # Fill in start
     clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((mailserver,port))
+    clientSocket.connect(mailserver)
 
     # Fill in end
 
@@ -30,7 +29,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
-    MAILFROMcommand = 'MAIL FROM: <alice@crepes.edu>\r\n'
+    MAILFROMcommand = 'MAIL FROM: <alice@crepes.com> \r\n'
     clientSocket.send(MAILFROMcommand.encode())
     recv2 = clientSocket.recv(1024).decode()
     print(recv2)
@@ -40,7 +39,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send RCPT TO command and handle server response.
     # Fill in start
-    RCPTTOcommand = 'RCPT TO: <bob@hamburger.edu>\r\n'
+    RCPTTOcommand = 'RCPT TO: <bob@hamburger.com>\r\n'
     clientSocket.send(RCPTTOcommand.encode())
     recv3 = clientSocket.recv(1024).decode()
     print(recv3)
@@ -60,12 +59,14 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send message data.
     # Fill in start
-    clientSocket.send(msg.encode())
+    clientSocket.send(bytes(msg.encode()))
+    recv7 = clientSocket.recv(1024).decode()
+    print(recv7)
     # Fill in end
 
     # Message ends with a single period, send message end and handle server response.
     # Fill in start
-    clientSocket.send(endmsg.encode())
+    clientSocket.send(bytes(endmsg.encode()))
     recv5 = clientSocket.recv(1024).decode()
     print(recv5)
     if recv5[:3] != '250':
