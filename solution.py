0abd9d5d4b3981d9e7a768a801a5791a35458f2a
spremarartna <95597829+spremarartna@users.noreply.github.com>
2022-02-24 19:16:55 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -82,6 +82,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     print(recv6)
     if recv6[:3] != '250':
         print('250 reply not received from server.')
+    clientSocket.close()
     # Fill in end
 
 
