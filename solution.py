71677348eac1994c670f26bb73db8cb87ee7f585
spremarartna <95597829+spremarartna@users.noreply.github.com>
2022-02-25 19:33:10 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,9 +9,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
     # Fill in start
-    mailserver = '127.0.0.1'
     clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((mailserver, 25))
+    clientSocket.bind(('127.0.0.1', 25))
 
     # Fill in end
 
